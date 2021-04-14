a8a0ce7060d8863888e6832676dc4b46bfc8c380
gabvrodrigues <gabriel.vieirarodrigues@gmail.com>
2021-04-13 22:16:38 -0300

    updating env

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+require('dotenv/config');
 const express = require('express');
 const app = express();
 
@@ -5,6 +6,6 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: false }));
 
 require('./routes/index')(app);
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 module.exports = app;
