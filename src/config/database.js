61d05bf305c75da48cd84a5821cd0a13966294f8
gabvrodrigues <gabriel.vieirarodrigues@gmail.com>
2021-04-13 23:39:57 -0300

    fixing database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,10 +1,10 @@
 require('dotenv/config');
 console.log("ENV", process.env.DATABASE_URL)
 module.exports = {
-    url: process.env.DATABASE_URL,
-    dialectOptions: {
-      ssl: { rejectUnauthorized: false }
-    },
-    dialect: 'postgres',
-    timezone: '-03:00',
+  use_env_variable: process.env.DATABASE_URL,
+  dialectOptions: {
+    ssl: { rejectUnauthorized: false }
+  },
+  dialect: 'postgres',
+  timezone: '-03:00',
 }
