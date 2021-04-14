390d0cf69ac85197e915e818208359f7ef659e20
gabvrodrigues <gabriel.vieirarodrigues@gmail.com>
2021-04-13 22:24:32 -0300

    updating database config

--- src/config/database.js
+++ src/config/database.js
@@ -2,10 +2,9 @@ require('dotenv/config');
 
 module.exports = {
   url: process.env.DATABASE_URL,
-  // username: process.env.DB_USERNAME,
-  // password: process.env.DB_PASSWORD,
-  // database: process.env.DB_DATABASE,
-  // host: process.env.DB_HOST,
+  dialectOptions: {
+    "ssl": true
+  },
   dialect: 'postgres',
   timezone: '-03:00'
 }
