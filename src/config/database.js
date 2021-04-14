d09f96346ad1e51bed8bc274f03e566da2718ecc
gabvrodrigues <gabriel.vieirarodrigues@gmail.com>
2021-04-13 23:04:19 -0300

    testing prod config

--- src/config/database.js
+++ src/config/database.js
@@ -1,13 +1,13 @@
 require('dotenv/config');
 console.log("ENV", process.env.DATABASE_URL)
 module.exports = {
-  "production": {
-    "use_env_variable": "DATABASE_URL",
+  production: {
+    use_env_variable: process.env.DATABASE_URL,
     timezone: '-03:00',
-    "dialect": "postgres",
-    "operatorsAliases": false
+    dialect: 'postgres',
+    operatorsAliases: false
   },
-  "development": {
+  development: {
     url: process.env.DATABASE_URL,
     dialectOptions: {
       ssl: { rejectUnauthorized: false }
